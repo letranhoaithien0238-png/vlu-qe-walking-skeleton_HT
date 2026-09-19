a87e3af09e8a7bdedeca7bd5361a86da9858fcd4
letranhoaithien0238-png <letranhoaithien0238@gmail.com>
2026-09-19 10:04:30 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,23 @@
+function login(user, pass) {
+    if (user === "admin" && pass === "123") {
+        return true;
+    }
 
+    return false;
+}
+
+function handleLogin() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+
+    if (login(user, pass)) {
+        document.getElementById("result").innerText = "Đăng nhập thành công";
+    } else {
+        document.getElementById("result").innerText = "Đăng nhập thất bại";
+    }
+}
+
+// Dùng cho Jest
+if (typeof module !== "undefined") {
+    module.exports = login;
+}
